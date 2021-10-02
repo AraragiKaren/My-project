0e65c5881c6f69bc8788d766f833700d09bcdae8
Koyomi <nhat>
2021-10-02 18:53:47 +0700

    add them

--- js/dance.js
+++ js/dance.js
@@ -1,11 +1,17 @@
 let container = document.getElementById("containerVid")
 
-function playVid() {
-    container.innerHTML ="<video width='100%' autoplay muted controls><source src='video/ei-chan.mp4' type='video/mp4'></video>"
-         
+function raidenSaySo() {
+    //container.innerHTML ="<video width='100%' autoplay muted controls><source src='video/ei-chan.mp4' type='video/mp4'></video>"
+    console.log(container.innerHTML =
+                            `<video width='100%' autoplay muted controls>
+                                <source src='video/ei-chan.mp4' type='video/mp4'>
+                            </video>`)
 }
-function anotherVid() {
-    container.innerHTML ="<video width='100%' autoplay muted controls><source src='video/raiden.mp4' type='video/mp4'></video>"
+function raidenRenai() {
+    container.innerHTML =
+                            `<video width='100%' autoplay muted controls>
+                                <source src='video/raiden.mp4' type='video/mp4'>
+                            </video>`
 }
-document.getElementById("watch").addEventListener("click", playVid)
-document.getElementById("next").addEventListener("click", anotherVid)
+document.getElementById("rai1").addEventListener("click", raidenSaySo)
+document.getElementById("rai2").addEventListener("click", raidenRenai)
